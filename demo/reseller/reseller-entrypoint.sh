30b4400e0d241027ace296bc2b9b56689229d893
Darshini Parikh <darshini.k.parikh@intel.com>
2020-11-21 21:53:04 +0530

    Enabling docker support for reseller.
    - Added docker files for reseller.
    - Updated reseller component sample pom to copy dependencies to demo
    folder.
    
    Signed-off-by: Darshini Parikh <darshini.k.parikh@intel.com>

---
+++ demo/reseller/reseller-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Copyright 2020 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+
+# Sample script that configures the softHSM and inserts the reseller keys
+
+# Initialize softHSM token with SO PIN and User pin.
+softhsm2-util --init-token --slot 0 --label "Reseller-Token" --so-pin $reseller_keystore_password --pin $reseller_keystore_password
+
+# Import owner keys into PKCS11 keystore
+keytool -importkeystore -deststorepass $reseller_keystore_password -destkeystore NONE -srckeystore reseller_keystore.p12 -deststoretype PKCS11 -srcstoretype PKCS12 -srcstorepass $reseller_keystore_password -alias reseller_pri
+
+# Delete the PKCS12 keystore as its not needed anymore
+rm -f reseller_keystore.p12
+
+# Start the owner service
+exec java -jar reseller.jar
