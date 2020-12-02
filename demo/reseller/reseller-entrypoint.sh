7bf6f10e1185b6eba34018090de305519cf347ed
Darshini Parikh <60647820+Darshini-Parikh93@users.noreply.github.com>
2020-12-02 12:54:33 +0530

    Fixing reseller alias name (#81)
    Fixed reseller alias name.
    Added reseller default keys for EC384 and RSA2048.
    - Updated keystore with new default keys
    - Updated docker entrypoint script for the same
    
    Signed-off-by: Darshini Parikh <darshini.k.parikh@intel.com>

--- demo/reseller/reseller-entrypoint.sh
+++ demo/reseller/reseller-entrypoint.sh
@@ -9,7 +9,7 @@
 softhsm2-util --init-token --slot 0 --label "Reseller-Token" --so-pin $reseller_keystore_password --pin $reseller_keystore_password
 
 # Import owner keys into PKCS11 keystore
-keytool -importkeystore -deststorepass $reseller_keystore_password -destkeystore NONE -srckeystore reseller_keystore.p12 -deststoretype PKCS11 -srcstoretype PKCS12 -srcstorepass $reseller_keystore_password -alias reseller_pri
+keytool -importkeystore -deststorepass $reseller_keystore_password -destkeystore NONE -srckeystore reseller_keystore.p12 -deststoretype PKCS11 -srcstoretype PKCS12 -srcstorepass $reseller_keystore_password
 
 # Delete the PKCS12 keystore as its not needed anymore
 rm -f reseller_keystore.p12
