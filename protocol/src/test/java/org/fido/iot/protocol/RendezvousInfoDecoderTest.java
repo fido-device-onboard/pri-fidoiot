c5364fdfa5dda20a22d4d15baac5642f64296002
Davis Benny <davis.benny@intel.com>
2021-03-10 11:52:34 +0530

    Updating directive variable
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- protocol/src/test/java/org/fido/iot/protocol/RendezvousInfoDecoderTest.java
+++ protocol/src/test/java/org/fido/iot/protocol/RendezvousInfoDecoderTest.java
@@ -11,13 +11,14 @@ public class RendezvousInfoDecoderTest {
   @Test
   void getHttpDirectivesTest() throws Exception {
 
-    String ownerdirective  = "http://localhost:8040?ipaddress=10.66.244.99&ownerport=8443";
-    Composite rvi = RendezvousInfoDecoder.decode(ownerdirective);
+    // localhost dnsname or localhost IP address can be replaced by any other IP
+    String directive  = "http://localhost:8040?ipaddress=127.0.0.1&ownerport=8443";
+
+    Composite rvi = RendezvousInfoDecoder.decode(directive);
     List<String> ownerDir = RendezvousInfoDecoder.getHttpDirectives(rvi, Const.RV_OWNER_ONLY);
     assertTrue(ownerDir.get(0).endsWith(":8443"));
 
-    String devdirective  = "https://localhost:8040?ipaddress=10.66.244.99&ownerport=8443";
-    Composite devRVI = RendezvousInfoDecoder.decode(devdirective);
+    Composite devRVI = RendezvousInfoDecoder.decode(directive);
     List<String> devDir =  RendezvousInfoDecoder.getHttpDirectives(devRVI, Const.RV_DEV_ONLY);
     assertTrue(devDir.get(0).endsWith(":8040"));
 
