6712e1e512f7aab19b58ff56b0369d288ec1de41
Davis Benny <52932399+DukeDavis12@users.noreply.github.com>
2021-03-10 23:50:34 +0530

    Adding unit tests to Protocol component (#157)
    * Adding unit tests to Protocol
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>
    
    * Updating directive variable
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- protocol/src/test/java/org/fido/iot/protocol/CompositeTest.java
+++ protocol/src/test/java/org/fido/iot/protocol/CompositeTest.java
@@ -3,9 +3,16 @@
 
 package org.fido.iot.protocol;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class CompositeTest {
@@ -19,6 +26,35 @@ public class CompositeTest {
     test.set(Const.FIRST_KEY, null);
     assertTrue(test.size() == 1);
     assertTrue(test.get(Const.FIRST_KEY) == null);
+
+  }
+
+  @Test
+  void decodeHexTest() {
+
+    byte[] arr = Composite.decodeHex("20");
+    assertArrayEquals(arr, new byte[]{ 32 });
+
+  }
+
+  @Test
+  void getAsMapTest() {
+
+    Map<Object,Object> input = new HashMap<Object, Object>();
+    input.put("K","V");
+    Composite test = Composite.newMap().set("K",input);
+    Map<Object,Object> res = test.getAsMap("K");
+    assertTrue(res.containsKey("K") && res.containsValue("V"));
+
+  }
+
+  @Test
+  void getAsCompositeTest() {
+
+    Composite obj = Composite.newArray().set(1 , "V");
+    Composite input = Composite.newArray().set(1, obj);
+    Composite res = input.getAsComposite(1);
+    assertTrue(res.get(1) == "V" );
 
   }
 }
