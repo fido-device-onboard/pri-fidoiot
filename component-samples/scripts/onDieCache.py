a3dc869073adf457e1c62e823980535151fde86d
Davis Benny <davis.benny@intel.com>
2021-06-01 11:58:06 +0530

    Fixing ondie path issues (#256)
      - Fix to download ondie .crts and .crls into the path specified by
        ondie_cache variable. Previously .crts and .crls were
        downloaded into the _JAVA_EXECUTION_PATH instead of the
        path specified by ondie_cache variable.
    
      - Fix to strip .new extension from the .crts and .crls files
        downloaded through onDieCache.py script.
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- component-samples/scripts/onDieCache.py
+++ component-samples/scripts/onDieCache.py
@@ -1,16 +1,23 @@
-#This script copies OnDie ECDSA artifacts from the cloud to a specified local directory.
-#Typically, the FDO components (Mfg, RV, Owner) will be configured to load OnDie certs from this
-#local directory. 
+# This script copies OnDie ECDSA artifacts from the cloud to a specified local directory.
+# Typically, the FDO components (Manufacturer, Owner) will be configured to load OnDie certs from this
+# local directory.
 #
-#When files are updated in the local directory, they will have a file type of ".new". In addition,
-#a touch file is created after copying to indicate to the component than an update has been made.
-#The component will trigger an update when this touch file is detected and will rename the .new 
-#files to the original names as the cache files are read. 
+# When files are updated in the local directory, they will have a file type of ".new". In addition,
+# a touch file is created after copying to indicate to the component than an update has been made.
+# The component will trigger an update when this touch file is detected and will rename the .new
+# files to the original names as the cache files are read.
 #
-#The use of the .new mechanism is employed to prevent any conflict between reading a file and 
-#copying a new version from the script.
+# The use of the .new mechanism is employed to prevent any conflict between reading a file and
+# copying a new version from the script.
 #
-#Use Python version 3.X with this script. This script is not compatible with earlier versions.
+# Use Python version 3.X with this script. This script is not compatible with earlier versions.
+#
+# USAGE:
+# 1. create the ondie_cache folder
+#     mkdir ondie_cache
+# 2. python3 onDieCache.py --cachedir ondie_cache
+# 3. copy the ondie_cache directory to demo/manufacturer and demo/owner.
+
 
 import sys, getopt
 import urllib.request
