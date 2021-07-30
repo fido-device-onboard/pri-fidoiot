b9481109ef38b888a34c96740148101c7c3f3c0c
Tushar Ranjan Behera <tushar.ranjan.behera@intel.com>
2021-07-30 17:27:03 +0530

    Cleanup unused flags in keys_gen script (#315)
    The script keys_gen.sh is further simplified and the additional flows
    to generate component specific artifacts has been removed.
    
    The script is further reorganized to group the functionalities together.
    The comments are updated to better reflect current usage.
    
    The TLS certificate generation is updated to include multiple IP/DNS
    addresses. This would help with multi-machine test setup.
    
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- component-samples/demo/scripts/keys_gen.sh
+++ component-samples/demo/scripts/keys_gen.sh
@@ -4,82 +4,59 @@
 # SPDX-License-Identifier: Apache 2.0
 #
 # Summary:
-#    keys_gen.sh script is mainly implemented to generate a new keystores,
-#    device keys, ssl keystore, ssl truststore for all PRI components to
-#    onboard PRI device or any other device with the fresh keys.
+# keys_gen.sh script is implemented to generate necessary keys and credentials
+# for different PRI components.
 #
-# List of output files:
+# These include following artifacts.
+# - Keystore files for AIO, Manuacturer, Reseller, Owner components
+# - TLS credentials (SSL keystore and truststore)
+# - Customer Public-key files AIO, Manufacturer, Reseller, Owner components
+# - Public-key hash values (for RV allow-list configuration)
+# - Password for keystore, truststore and REST APIs updated in respective
+# creds.env file
 #
-# PRI FIDOIOT component output files. xxx -> [manufacturer, owner, reseller]
-#
-# /path/to/root/of/pri/binaries/creds/xxx_pub_keys.pem  -> Public Keys file
-# /path/to/root/of/pri/binaries/creds/xxx_pub_keys.hash -> Hash file
-# /path/to/root/of/pri/binaries/creds/xxx_keystore.p12  -> Keystore file
-#
-# TLS truststore and keystore output files.
-#
-# /path/to/root/of/pri/binaries/creds/ssl.p12    -> SSL Keystore
-# /path/to/root/of/pri/binaries/creds/truststore -> Truststore
-#
-# PRI Device pem output files. xxx -> [256, 384]
-# /path/to/root/of/pri/binaries/creds/device_ecxxx_cert.pem    -> Device certificate
-# /path/to/root/of/pri/binaries/creds/device_ecxxx_private.key -> Device private key
-#
-# Commands we use for execution:
-# 1) ./keys_gen.sh
-#   Gives a message to make use of -h option
-# 2) ./keys_gen.sh -h
-#   Displays the usage message with all available options
-# 3) ./keys_gen.sh /path/to/root/of/pri/binaries
-#   Generates new credentials and update all destination paths
-# 4) ./keys_gen.sh /path/to/root/of/pri/binaries -m
-#   Generates new credentials for manufacturer and update all files within manufacturer component
-# 5) ./keys_gen.sh /path/to/root/of/pri/binaries -o
-#   Generates new credentials for owner and update all files within owner component
-# 6) ./keys_gen.sh /path/to/root/of/pri/binaries -r
-#   Generates new credentials for reseller and update all files within reseller component
-# 7) ./keys_gen.sh /path/to/root/of/pri/binaries -t
-#   Generates new ssl keystore and its truststore for signing and update all destination paths
-# 8) ./keys_gen.sh /path/to/root/of/pri/binaries -d
-#   Generates new device keys and updates all files.
-# 9) ./keys_gen.sh /path/to/root/of/pri/binaries <combination of options>
-#   Generates the new credentials based upon the options that we provide in a combination
-#
-# ToDo List:
-# 1) Hashes update in the file component-samples/demo/rv/config.properties should reflect in RV database
-# 2) Removing the existing credentials from the source code
-# 3) Updating the code to read from the newly generated keys by overwriting the default keys
-#
-# Validated Scenarios with these new set of credentials:
-# 1) Basic sanity testcase (e2e)
-# 2) Resale testcase
-# 3) Reuse testcase
-# 4) SVI testcase
-# 5) RV-ByPass testcase
-# 6) Multiple RV-Info testcase
-# 7) MTU testcase
-# 8) Basic e2e testcase with HTTPS
+# Following tests have been performed with the generated credentials.
+# - Sanity end-to-end(E2E) with HTTP
+# - Sanity E2E with HTTPS
+# - Resale scenario with Owner / Owner2 keys
+# - E2E with Size-2 voucher (Manufacturer -> Reseller -> Owner)
+# - Multi-machine E2E with HTTPS
 
 shopt -s extglob
 set -e
 
+########
+# Help #
+########
+
 # Usage message to be displayed whenever we provide wrong inputs
 usage()
 {
-  echo -e "Usage:
-  $0 <absolute_path_of_pri_components> <OPTIONS>\n
+  echo -e "
+  Usage:
+    $0 [destdir | -h]
 
-  absolute_path_of_pri_components => /path/to/root/of/pri/binaries
+      destdir: Credentials are created destdir/creds folder.
+               destdir can be either a relative path or an absolute path.
+               destdir is created if not present.
+               Defaults to present working directory if not present.
+      -h: Prints help message.
 
-  OPTIONS:
-    -m => To create manufacturer keystore
-    -o => To create owner keystore
-    -r => To create reseller keystore
-    -t => To create TLS keystore
-    -d => To create device pem files
-    -h => Prints help message\n
-  NOTE: Order of arguments passing shouldn't change"
+  Examples:
+
+    $ bash keys_gen.sh
+    Creates the credentials folder 'creds' in current director.
+
+    $ bash keys_gen.sh destdir
+    Creates the credentials folder 'creds' in 'destdir'.
+
+    $ bash keys_gen.sh -h
+    Prints the help message."
 }
+
+#######################################
+# Helper function for env file update #
+#######################################
 
 # Update the component creds.env file with the new credentials
 pass_update_env_files()
@@ -94,16 +71,9 @@ pass_update_env_files()
   echo ${statement} >> ${comp}/creds.env
 }
 
-# Generation of keystore with rsa, ec256 and ec384 keys as shown below
-# ---------------------------------------------------------------------------
-# Private key    -> Public Key    -> Certificate -> P12 Key   -> P12 Keystore
-# ---------------------------------------------------------------------------
-# rsa_priv_key   -> rsa_pub_key   -> rsa_cert    -> rsa.p12   -> keystore.p12
-# ec256_priv_key -> ec256_pub_key -> ec256_cert  -> ec256.p12 -> keystore.p12
-# ec384_priv_key -> ec384_pub_key -> ec384_cert  -> ec384.p12 -> keystore.p12
-# ---------------------------------------------------------------------------
-# Merging the above generated public keys into one file with respective to single pri component
-# Redirecting the randomly generated password into /path/to/root/of/pri/binaries/creds/pri_keys.pass
+#######################################
+# Helper functions for key generation #
+#######################################
 
 # Generate hash of public key (takes certificate as input)
 # Certificate -> Encrypted .der file -> Hash
@@ -234,6 +204,19 @@ import_keystore()
     -deststoretype pkcs12 -noprompt > /dev/null 2>&1
 }
 
+#####################################
+# Component specific key generation #
+#####################################
+
+# Generation of keystore with RSA2048, EC256 and EC384 keys as shown below
+# ---------------------------------------------------------------------------
+# Private key    -> Public Key    -> Certificate -> P12 Key   -> P12 Keystore
+# ---------------------------------------------------------------------------
+# rsa_priv_key   -> rsa_pub_key   -> rsa_cert    -> rsa.p12   -> keystore.p12
+# ec256_priv_key -> ec256_pub_key -> ec256_cert  -> ec256.p12 -> keystore.p12
+# ec384_priv_key -> ec384_pub_key -> ec384_cert  -> ec384.p12 -> keystore.p12
+# ---------------------------------------------------------------------------
+# Merging the above generated public keys into one file with respective to single component
 generate_keystore()
 {
   comp=${1}
@@ -279,10 +262,8 @@ generate_keystore()
   echo "${PASS_KEY}" > ${1}_keystore.pass
 }
 
-# Calling the generate_keystore functions generate keystores and hashes
-# Updating the existing component keystores with the newly generated keystores
-# Updating the corresponding passwords in all the component environment files
-# Logging the path of the generated keystores or hashes or passwords
+# Generate component specific keystores and update the password in creds.env
+# file
 generate_component_keys()
 {
   # Cleaning all component keys that only exists in order to regenerate fresh keys
@@ -306,8 +287,101 @@ generate_component_keys()
   printf " successful\n"
 }
 
+# Generation of TLS keystore and truststore
+generate_tls_keystore()
+{
+  # Cleaning all existing component keys to regenerate fresh keys
+  rm -f ssl.p12 ssl.pass truststore
+
+  printf "%.40s" "Generating ssl.p12 and truststore ..................."
+
+  # Configuring list of IPs
+  # Get IP of current machine
+  if=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
+  ip=$(ip addr | grep inet | grep " $if" | cut -d' ' -f6 | cut -d'/' -f1)
+
+  # The SSL certificate would include all these DNS/IP in the subjectAlName field
+  # Apppend additional DNSs at the end within double-quotes
+  # Example: dns_list=("rv1.fdo" "rv2.fdo")
+  # Apppend additional IPs at the end within double-quotes
+  # Example: ip_list=("${ip}" "127.0.0.1" "192.168.0.1)
+  subjectAltName="DNS:localhost"
+  dns_list=()
+  ip_list=("${ip}" "127.0.0.1")
+  for i in ${dns_list[@]}; do
+    subjectAltName+=",DNS:${i}"
+  done
+  for i in ${ip_list[@]}; do
+    subjectAltName+=",IP:${i}"
+  done
+
+  # TLS keystore and truststore creation steps
+  openssl req \
+    -x509 \
+    -newkey rsa:2048 \
+    -sha256 \
+    -days 3560 \
+    -nodes \
+    -keyout tls.key \
+    -out tls.crt \
+    -subj '/CN=fdo' \
+    -extensions san \
+    -config <( \
+    echo '[req]'; \
+    echo 'distinguished_name=req'; \
+    echo '[san]'; \
+    echo 'subjectAltName='${subjectAltName}) > /dev/null 2>&1
+
+  export SSL_PASS=`openssl rand --base64 8 | tr -dc 0-9A-Za-z`
+  openssl pkcs12 -export -in tls.crt -inkey tls.key -out ssl.p12 -password pass:${SSL_PASS}
+  keytool -import -alias fdo -file tls.crt -storetype PKCS12 -keystore truststore \
+    -storepass ${SSL_PASS} -noprompt > /dev/null 2>&1
+  echo "${SSL_PASS}" > ssl.pass
+
+  # Copying the ssl keystore and truststores to the destination paths
+  for comp in "manufacturer" "rv" "owner" "reseller" "aio"; do
+    mkdir -p $CREDS_PATH/$comp/certs/
+    cp ssl.p12 $CREDS_PATH/$comp/certs/
+    pass_update_env_files $CREDS_PATH/$comp "${comp}_ssl_keystore-password=${SSL_PASS}"
+  done
+
+  for comp in "owner" "aio"; do
+    mkdir -p $CREDS_PATH/$comp/certs
+    cp truststore $CREDS_PATH/$comp/certs/
+    pass_update_env_files $CREDS_PATH/$comp "ssl_truststore_password=${SSL_PASS}"
+  done
+
+  printf " successful\n"
+
+  rm -rf tls.???
+}
+
+# Generating new device keys and updating it into the device.pem file
+device_pem_files()
+{
+  mkdir -p $CREDS_PATH/device
+
+  printf "%.40s" "Generating device keys ..................."
+  generate_keypair_ec 256
+  cat cert.pem > $CREDS_PATH/device/device_ec256.pem
+  cat private.key >> $CREDS_PATH/device/device_ec256.pem
+
+  generate_keypair_ec 384
+  cat cert.pem > $CREDS_PATH/device/device_ec384.pem
+  cat private.key >> $CREDS_PATH/device/device_ec384.pem
+  printf " successful\n"
+
+  cp $CREDS_PATH/device/device_ec256.pem $CREDS_PATH/device/device.pem
+}
+
+#####################################
+# Component specific configurations #
+#####################################
+
+# Generate password for REST API authentication
 generate_api_pass()
 {
+  printf "%.40s" "Generating API password .........................."
   api_pass=`openssl rand --base64 12 | tr -dc 0-9A-Za-z`
 
   # Update API password
@@ -315,12 +389,13 @@ generate_api_pass()
     pass_update_env_files $CREDS_PATH/$comp "${comp}_api_password=${api_pass}"
   done
 
+  printf " successful\n"
 }
 
 # Updating all component key hashes to the RV config.properties
 prepare_rv_config()
 {
-  printf "%.40s" "Updating rv/config.properties ..................."
+  printf "%.40s" "Generating rv/config.properties ..................."
   # Capturing all component keystore hashes into one environmental variable HASHES
   for i in *.hash; do
     if [[ $i =~ ".hash" ]]; then
@@ -346,78 +421,9 @@ prepare_rv_config()
   printf " successful\n"
 }
 
-# Generating new device keys and updating it into the device.pem file
-device_pem_files()
-{
-  mkdir -p $CREDS_PATH/device
-
-  printf "%.40s" "Generating device_ec256.pem ..................."
-  generate_keypair_ec 256
-  cat cert.pem > $CREDS_PATH/device/device_ec256.pem
-  cat private.key >> $CREDS_PATH/device/device_ec256.pem
-  printf " successful\n"
-
-  printf "%.40s" "Generating device_ec384.pem ..................."
-  generate_keypair_ec 384
-  cat cert.pem > $CREDS_PATH/device/device_ec384.pem
-  cat private.key >> $CREDS_PATH/device/device_ec384.pem
-  printf " successful\n"
-
-  cp $CREDS_PATH/device/device_ec256.pem $CREDS_PATH/device/device.pem
-}
-
-# Generation of TLS keystore and truststore for signing purpose
-# Updating all the keystores and truststores in the required destination paths
-# Updating the ssl-keystore-password that is generated randomly in all the component environment files
-generate_tls_keystore()
-{
-  # Cleaning all existing component keys to regenerate fresh keys
-  rm -f ssl.p12 ssl.pass truststore
-
-  printf "%.40s" "Generating ssl.p12 and truststore ..................."
-
-  # TLS keystore and truststore creation steps
-  if=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
-  ip=$(ip addr | grep inet | grep " $if" | cut -d' ' -f6 | cut -d'/' -f1)
-  openssl req \
-    -x509 \
-    -newkey rsa:2048 \
-    -sha256 \
-    -days 3560 \
-    -nodes \
-    -keyout tls.key \
-    -out tls.crt \
-    -subj '/CN=fdo' \
-    -extensions san \
-    -config <( \
-    echo '[req]'; \
-    echo 'distinguished_name=req'; \
-    echo '[san]'; \
-    echo 'subjectAltName=DNS:localhost,IP.1:127.0.0.1,IP.2:'${ip}) > /dev/null 2>&1
-
-  export SSL_PASS=`openssl rand --base64 8 | tr -dc 0-9A-Za-z`
-  openssl pkcs12 -export -in tls.crt -inkey tls.key -out ssl.p12 -password pass:${SSL_PASS}
-  keytool -import -alias fdo -file tls.crt -storetype PKCS12 -keystore truststore \
-    -storepass ${SSL_PASS} -noprompt > /dev/null 2>&1
-  echo "${SSL_PASS}" > ssl.pass
-
-  # Copying the ssl keystore and truststores to the destination paths
-  for comp in "manufacturer" "rv" "owner" "reseller" "aio"; do
-    mkdir -p $CREDS_PATH/$comp/certs/
-    cp ssl.p12 $CREDS_PATH/$comp/certs/
-    pass_update_env_files $CREDS_PATH/$comp "${comp}_ssl_keystore-password=${SSL_PASS}"
-  done
-
-  for comp in "owner" "aio"; do
-    mkdir -p $CREDS_PATH/$comp/certs
-    cp truststore $CREDS_PATH/$comp/certs/
-    pass_update_env_files $CREDS_PATH/$comp "ssl_truststore_password=${SSL_PASS}"
-  done
-
-  printf " successful\n"
-
-  rm -rf tls.???
-}
+#########################################
+# Copy artifacts to destination folders #
+#########################################
 
 # Copy only if source file exists
 # Create destination folder if it doesn't exist
@@ -445,6 +451,7 @@ copy_failsafe()
   copy_failsafe_rename $1 $2 $1
 }
 
+# Copy remaining files that didn't get copied through earlier calls.
 misc_copy()
 {
   printf "%.40s" "Copying files to components ..................."
@@ -479,74 +486,49 @@ misc_copy()
   printf " successful\n"
 }
 
+############################
+# Top level function calls #
+############################
+
 # Main functionality is defined in this function
 start_generation()
 {
-  arr=($*)
   components=("manufacturer" "reseller" "owner")
-  if [[ $# == 0 ]]; then
-    echo "For help, provide -h to the script"; exit 1;
-  elif [[ $* =~ "-h" ]]; then
-    usage; exit 1;
-  elif [[ $# == 1 ]]; then
-    arr+=('default')
-  fi
-  for option in ${arr[@]:1}; do
-    case $option in
-      "-m")
-        generate_component_keys "manufacturer"
-        prepare_rv_config
-        misc_copy
-        ;;
-      "-o")
-        generate_component_keys "owner"
-        prepare_rv_config
-        misc_copy
-        ;;
-      "-r")
-        generate_component_keys "reseller"
-        prepare_rv_config
-        misc_copy
-        ;;
-      "-t")
-        generate_tls_keystore
-        ;;
-      "-d")
-        device_pem_files
-        ;;
-      *)
-        for i in ${components[@]}; do
-          if [[ "${components[@]}" =~ "$i" ]]; then
-            generate_component_keys $i
-          fi
-        done
-        generate_api_pass
-        prepare_rv_config;
-        generate_tls_keystore
-        device_pem_files
-        misc_copy
-        break;;
-    esac
+  for i in ${components[@]}; do
+    if [[ "${components[@]}" =~ "$i" ]]; then
+      generate_component_keys $i
+    fi
   done
+  generate_api_pass
+  prepare_rv_config;
+  generate_tls_keystore
+  device_pem_files
+  misc_copy
 }
 
 # Environmental variables
-if [ $# -lt 1 ]; then
+if [ $# -gt 1 ]; then
   usage
   exit
 fi
 
-COMP_PATH=$(realpath $1)
+COMP_PATH=`pwd`
+if [ $# -eq 1 ]; then
+  if [ $1 == "-h" ]; then
+    usage
+    exit
+  fi
+  COMP_PATH=$(realpath $1)
+fi
 CREDS_PATH=$COMP_PATH/creds
 CERTS_PATH=$CREDS_PATH/tmp
 
-# Change work directory to CERTS_PATH
 rm -rf $CERTS_PATH $CREDS_PATH
 mkdir -p $CERTS_PATH $CREDS_PATH
-
 cd $CERTS_PATH
 
-start_generation $*
+start_generation
 
 # Remove temporary files
 rm -rf $CERTS_PATH
+echo "Generated artifacts are available in '"$CREDS_PATH"'"
