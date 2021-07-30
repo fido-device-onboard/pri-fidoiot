6e989894a2b37b672b758a2ac86f3d80944912fb
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-07-30 15:55:15 +0530

    Add log messages
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- component-samples/aio/src/main/java/org/fidoalliance/fdo/sample/AioRegisterBlobServlet.java
+++ component-samples/aio/src/main/java/org/fidoalliance/fdo/sample/AioRegisterBlobServlet.java
@@ -1,47 +1,37 @@
 package org.fidoalliance.fdo.sample;
 
-import java.io.IOException;
 import java.util.function.Consumer;
-import javax.servlet.AsyncContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.fidoalliance.fdo.loggingutils.LoggerService;
+
 public class AioRegisterBlobServlet extends HttpServlet {
 
+  LoggerService logger = new LoggerService(AioRegisterBlobServlet.class);
   public static final String BLOB_INJECTOR = "To0BlobInjector";
 
-  private void getAsync(AsyncContext asyncCtx) {
-
-    final HttpServletRequest req = (HttpServletRequest) asyncCtx.getRequest();
-    final HttpServletResponse resp = (HttpServletResponse) asyncCtx.getResponse();
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 
     final int pos = req.getRequestURI().lastIndexOf('/');
     final String guid = req.getRequestURI().substring(pos + 1);
+
     if (guid.startsWith(".") || guid.indexOf('\\') > 0) {
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      asyncCtx.complete();
+      logger.warn("Received invalid guid: " + guid);
+      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
       return;
     }
 
     try {
-      Consumer<String> injector = (Consumer<String>) req.getServletContext()
-          .getAttribute(BLOB_INJECTOR);
+      Consumer<String> injector =
+          (Consumer<String>) req.getServletContext().getAttribute(BLOB_INJECTOR);
       injector.accept(guid);
-    } finally {
-      asyncCtx.complete();
+      logger.info("Registered guid: " + guid);
+    } catch (Exception ex) {
+      logger.warn("Failed to register guid: " + guid);
+      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
     }
-
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-
-    final AsyncContext asyncCtx = req.startAsync();
-    asyncCtx.setTimeout(0);
-    new Thread(() -> getAsync(asyncCtx)).start();
-
   }
 }
