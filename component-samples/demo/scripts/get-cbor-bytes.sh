f617495abf9dddbbd9931a1d02c33221be084f82
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-08-04 09:38:52 +0530

    Update default RVInfo blob in script
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- component-samples/demo/scripts/get-cbor-bytes.sh
+++ component-samples/demo/scripts/get-cbor-bytes.sh
@@ -5,17 +5,17 @@
 #
 # Example of single RV information:
 #
-#   ./get-cbor-bytes.sh "[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8443]]]"
+#   ./get-cbor-bytes.sh "[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8041]]]"
 #
 #   NOTE : Escape double quotes characters.
 #   NOTE: here 7F000001 is the hex representation of the IP address 127(7F).0(00).0(00).1(01)
 #
 # Example of multiple RV instructions:
-#     ./get-cbor-bytes.sh "[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8443]],
-#                   [[5, \"localhost\"], [3, 8040], [12, 1], [2, h'6A140101'], [4, 8443]]]"
+#     ./get-cbor-bytes.sh "[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8041]],
+#                   [[5, \"localhost\"], [3, 8040], [12, 1], [2, h'6A140101'], [4, 8041]]]"
 #
 
-default_diagnostic="[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8443]]]"
+default_diagnostic="[[[5, \"localhost\"], [3, 8040], [12, 1], [2, h'7F000001'], [4, 8041]]]"
 diagnostic=${1:-$default_diagnostic}
 parameterized_diagnostic=`echo $diagnostic | sed 's/ /%20/g' | sed 's/"/%22/g' | sed "s/'/%27/g"`
 result=`curl http://cbor.me/?diag=${parameterized_diagnostic} --globoff `
