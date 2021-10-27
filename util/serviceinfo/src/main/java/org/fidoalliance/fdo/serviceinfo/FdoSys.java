ef32db652ef8a8a1f3f79a039a4b8a55d6a579f4
Templeton, Randall <randall.f.templeton@intel.com>
2021-10-26 18:50:12 -0700

    added keep alive message processing to SysModule
    Signed-off-by: Templeton, Randall <randall.f.templeton@intel.com>

--- util/serviceinfo/src/main/java/org/fidoalliance/fdo/serviceinfo/FdoSys.java
+++ util/serviceinfo/src/main/java/org/fidoalliance/fdo/serviceinfo/FdoSys.java
@@ -14,11 +14,15 @@ public final class FdoSys {
   public static final String NAME = "fdo_sys";
   public static final String KEY_ACTIVE = NAME + ":active";
   public static final String KEY_EXEC = NAME + ":exec";
+  public static final String KEY_EXEC_CB = NAME + ":exec_cb";
+  public static final String KEY_STATUS_CB = NAME + ":status_cb";
+  public static final String KEY_FETCH = NAME + ":fetch";
+  public static final String KEY_DATA = NAME + ":data";
+  public static final String KEY_EOT = NAME + ":eot";
   public static final String KEY_FILEDESC = NAME + ":filedesc";
   public static final String KEY_WRITE = NAME + ":write";
-  public static final String KEY_KEEP_ALIVE = NAME + ":keepalive";
-  public static final String KEY_RET_CODE = NAME + ":retcode";
   public static final String KEY_IS_DONE = NAME + ":isdone";
   public static final String KEY_IS_MORE = NAME + ":ismore";
+
 }
 
