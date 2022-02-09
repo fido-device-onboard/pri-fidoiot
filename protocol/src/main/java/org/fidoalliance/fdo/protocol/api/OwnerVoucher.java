86bd01b8d5845fb6177699b24e660be7614c376f
Davis Benny <davis.benny@intel.com>
2022-02-09 18:38:54 +0530

    E2E demo
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/api/OwnerVoucher.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/api/OwnerVoucher.java
@@ -13,11 +13,7 @@ import org.fidoalliance.fdo.protocol.db.OnboardConfigSupplier;
 import org.fidoalliance.fdo.protocol.dispatch.VoucherQueryFunction;
 import org.fidoalliance.fdo.protocol.entity.OnboardingConfig;
 import org.fidoalliance.fdo.protocol.entity.OnboardingVoucher;
-import org.fidoalliance.fdo.protocol.message.OwnershipVoucher;
-import org.fidoalliance.fdo.protocol.message.OwnershipVoucherHeader;
-import org.fidoalliance.fdo.protocol.message.To0OwnerSign;
-import org.fidoalliance.fdo.protocol.message.To0d;
-import org.fidoalliance.fdo.protocol.message.To2AddressEntries;
+import org.fidoalliance.fdo.protocol.message.*;
 
 public class OwnerVoucher extends RestApi {
 
@@ -25,24 +21,31 @@ public class OwnerVoucher extends RestApi {
   @Override
   public void doPost() throws Exception {
 
+    getTransaction();
     OwnershipVoucher voucher = VoucherUtils.fromString(getStringBody());
     OwnershipVoucherHeader header = Mapper.INSTANCE.readValue(voucher.getHeader(),
         OwnershipVoucherHeader.class);
     byte[] data = Mapper.INSTANCE.writeValue(voucher);
+    String guid  = header.getGuid().toString();
 
     OnboardingVoucher onboardingVoucher = getSession().get(OnboardingVoucher.class,
         header.getGuid().toString());
+
     if (onboardingVoucher != null) {
-      VoucherUtils.fromString(getStringBody());
+      //VoucherUtils.fromString(getStringBody());
+      onboardingVoucher.setGuid(guid.toString());
       onboardingVoucher.setData(data);
       getSession().update(onboardingVoucher);
+      getTransaction().commit();
     } else {
-      VoucherUtils.fromString(getStringBody());
+      //VoucherUtils.fromString(getStringBody());
       onboardingVoucher = new OnboardingVoucher();
+      onboardingVoucher.setGuid(guid);
       onboardingVoucher.setData(data);
       onboardingVoucher.setTo0Expiry(new Date(System.currentTimeMillis()));
       onboardingVoucher.setCreatedOn(new Date(System.currentTimeMillis()));
       getSession().save(onboardingVoucher);
+      getTransaction().commit();
     }
     getResponse().setContentType(HttpUtils.HTTP_PLAIN_TEXT);
     getResponse().getWriter().print(header.getGuid().toString());
