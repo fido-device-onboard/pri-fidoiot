f0180fbdbff77e9712bd2d3770056b01f36dcbf6
Davis Benny <davis.benny@intel.com>
2022-02-10 12:14:11 +0530

    Minor fixes to OwnerVoucher API
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/api/OwnerVoucher.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/api/OwnerVoucher.java
@@ -25,24 +25,30 @@ public class OwnerVoucher extends RestApi {
   @Override
   public void doPost() throws Exception {
 
+    getTransaction();
     OwnershipVoucher voucher = VoucherUtils.fromString(getStringBody());
     OwnershipVoucherHeader header = Mapper.INSTANCE.readValue(voucher.getHeader(),
         OwnershipVoucherHeader.class);
     byte[] data = Mapper.INSTANCE.writeValue(voucher);
+    String guid  = header.getGuid().toString();
 
     OnboardingVoucher onboardingVoucher = getSession().get(OnboardingVoucher.class,
         header.getGuid().toString());
     if (onboardingVoucher != null) {
-      VoucherUtils.fromString(getStringBody());
+
+      onboardingVoucher.setGuid(guid.toString());
       onboardingVoucher.setData(data);
       getSession().update(onboardingVoucher);
+      getTransaction().commit();
     } else {
-      VoucherUtils.fromString(getStringBody());
+
       onboardingVoucher = new OnboardingVoucher();
+      onboardingVoucher.setGuid(guid.toString());
       onboardingVoucher.setData(data);
       onboardingVoucher.setTo0Expiry(new Date(System.currentTimeMillis()));
       onboardingVoucher.setCreatedOn(new Date(System.currentTimeMillis()));
       getSession().save(onboardingVoucher);
+      getTransaction().commit();
     }
     getResponse().setContentType(HttpUtils.HTTP_PLAIN_TEXT);
     getResponse().getWriter().print(header.getGuid().toString());
