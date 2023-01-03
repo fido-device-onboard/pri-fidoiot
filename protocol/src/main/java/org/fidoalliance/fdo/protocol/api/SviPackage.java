c4a921095e371f68c22ffddd4839089c8fc28325
Peeush Thakur <119664969+tPeeush@users.noreply.github.com>
2023-01-03 12:34:46 +0530

    Update SviPackage.java

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/api/SviPackage.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/api/SviPackage.java
@@ -28,7 +28,7 @@ public class SviPackage extends RestApi {
       String result = systemPackage.getData().getSubString(1,
           Long.valueOf(systemPackage.getData().length()).intValue());
       getResponse().getWriter().print(result);
-      logger.info("SVI package result : " + result);
+      logger.debug("SVI package result : " + result);
     }
   }
 
