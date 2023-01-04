5f059c1f182769dc7bed8535d132d7e49759bc7a
Davis Benny <davis.benny@intel.com>
2023-01-04 11:27:07 +0530

    Updating healthCheck status for PRI components
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/api/HealthApi.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/api/HealthApi.java
@@ -4,8 +4,11 @@
 package org.fidoalliance.fdo.protocol.api;
 
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.net.URL;
 import java.util.Date;
 
+import org.fidoalliance.fdo.protocol.EpidService;
 import org.fidoalliance.fdo.protocol.LoggerService;
 
 
@@ -20,6 +23,10 @@ import org.fidoalliance.fdo.protocol.LoggerService;
 
 public class HealthApi extends RestApi {
 
+  public String databaseHealthStatus() {
+    return getTransaction().getStatus().toString();
+  }
+
   @Override
   public void doGet() throws Exception {
 
@@ -27,15 +34,18 @@ public class HealthApi extends RestApi {
 
     try {
 
+      final String serviceStatus = "OK";
       String systemTime = new Date().toString();
       logger.info("Health check invoked at " + systemTime);
-      String responseBody = "{\"version\" : \"%s\", \"status\" : \"OK\"}";
+
+      final String dbHealth = databaseHealthStatus();
+      String responseBody = "{\"version\" : \"%s\", \"databaseConnection\" : \"%s\", \"status\" : \"%s\"}";
 
       // Collects property from the service.yml file.
       String appVersion = System.getProperty("application.version");
 
       // Appends responseBody to the outputStream of HttpResponse Object.
-      getResponse().getWriter().write(String.format(responseBody, appVersion));
+      getResponse().getWriter().write(String.format(responseBody, appVersion, dbHealth, serviceStatus));
       getResponse().setContentType("application/json");
 
     } catch (Exception e) {
