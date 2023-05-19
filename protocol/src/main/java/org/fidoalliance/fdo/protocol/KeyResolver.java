ab38d7462fa1e21bcb1b14340551fec1924c2cc4
Randy Templeton <47697438+rftemple@users.noreply.github.com>
2023-05-19 00:32:45 -0700

    Fixing Certificate Chain (#608)
    * added root CA extension so cert chain will verify with openssl verify command
    * added PKIX Java cert chain validation
    * fixed setting CA attribute for PKIX
    
    Signed-off-by: Templeton, Randall <randall.f.templeton@intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/KeyResolver.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/KeyResolver.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.NoSuchElementException;
-import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.asn1.x509.BasicConstraints;
 import org.fidoalliance.fdo.protocol.Config.KeyStoreConfig;
 import org.fidoalliance.fdo.protocol.dispatch.CryptoService;
 import org.fidoalliance.fdo.protocol.dispatch.KeyStoreInputStreamFunction;
@@ -82,8 +82,9 @@ public class KeyResolver {
           .setPublicKey(keyPair.getPublic())
           .setProvider(cs.getProvider())
           .setSignatureAlgorithm(sigAlgorithm)
-          .setSubject("CN=FdoEntity")
+          .setSubject("CN=FdoEntity CA")
           .setValidityDays(Config.getWorker(ValidityDaysSupplier.class).get())
+          .setCA(true)
           .build();
 
       keyStore.setKeyEntry(KeyResolver.getAlias(keyType, sizeType),
@@ -100,6 +101,7 @@ public class KeyResolver {
 
   /**
    * Gets the underlying keystore.
+   *
    * @return The underlying keystore.
    */
   public KeyStore getKeyStore() {
@@ -122,12 +124,12 @@ public class KeyResolver {
         String pemString = Files.readString(Path.of(config.getPath()));
         List<Certificate> certs = PemLoader.loadCerts(pemString);
         final String keyPass = config.getPassword();
-        PrivateKey privateKey = PemLoader.loadPrivateKey(pemString,keyPass);
+        PrivateKey privateKey = PemLoader.loadPrivateKey(pemString, keyPass);
 
         this.keyStore = KeyStore.getInstance("JKS");
-        this.keyStore.load(null,getPasswordArray());
+        this.keyStore.load(null, getPasswordArray());
 
-        this.keyStore.setKeyEntry(config.getAlias(),privateKey,getPasswordArray(),
+        this.keyStore.setKeyEntry(config.getAlias(), privateKey, getPasswordArray(),
             certs.stream().toArray(Certificate[]::new));
 
 
