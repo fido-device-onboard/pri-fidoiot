3128fb3811b96ecc44c3b85f2a911f5cb8fa5de1
root <root@surajku3-mobl.gar.corp.intel.com>
2024-01-29 11:57:35 +0530

    Fixing empty deviceInfo issue in DI Message 10(corrected format)
    Signed-off-by: root <root@surajku3-mobl.gar.corp.intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/serialization/ManufacturingInfoDeserializer.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/serialization/ManufacturingInfoDeserializer.java
@@ -58,7 +58,6 @@ public class ManufacturingInfoDeserializer extends StdDeserializer<Manufacturing
     
     int index = 0;
     ManufacturingInfo info = new ManufacturingInfo();
-    
     info.setKeyType(PublicKeyType.fromNumber(node.get(index++).intValue()));
     info.setKeyEnc(PublicKeyEncoding.fromNumber(node.get(index++).intValue()));
 
