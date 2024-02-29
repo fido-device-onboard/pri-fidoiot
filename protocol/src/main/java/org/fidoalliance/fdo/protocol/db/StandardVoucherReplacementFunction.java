5c3b2a9aa5b179f4f02b167e0320921b0fad28d8
Benny <davis.benny@intel.com>
2024-02-29 13:19:31 +0530

    Adding REST endpoints to support Replacement RVInfo
      Adding REST endpoints to support Replacement RVInfo.
    
    Signed-off-by: Benny <davis.benny@intel.com>

--- protocol/src/main/java/org/fidoalliance/fdo/protocol/db/StandardVoucherReplacementFunction.java
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/db/StandardVoucherReplacementFunction.java
@@ -4,6 +4,8 @@
 package org.fidoalliance.fdo.protocol.db;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import org.fidoalliance.fdo.protocol.AlgorithmFinder;
 import org.fidoalliance.fdo.protocol.Config;
 import org.fidoalliance.fdo.protocol.KeyResolver;
@@ -11,10 +13,14 @@ import org.fidoalliance.fdo.protocol.Mapper;
 import org.fidoalliance.fdo.protocol.dispatch.CryptoService;
 import org.fidoalliance.fdo.protocol.dispatch.ReplacementKeySupplier;
 import org.fidoalliance.fdo.protocol.dispatch.VoucherReplacementFunction;
+import org.fidoalliance.fdo.protocol.entity.OnboardingConfig;
 import org.fidoalliance.fdo.protocol.message.Guid;
 import org.fidoalliance.fdo.protocol.message.OwnerPublicKey;
 import org.fidoalliance.fdo.protocol.message.OwnershipVoucher;
 import org.fidoalliance.fdo.protocol.message.OwnershipVoucherHeader;
+import org.fidoalliance.fdo.protocol.message.RendezvousInfo;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class StandardVoucherReplacementFunction implements VoucherReplacementFunction {
 
@@ -27,15 +33,35 @@ public class StandardVoucherReplacementFunction implements VoucherReplacementFun
         Mapper.INSTANCE.readValue(headerTag, OwnershipVoucherHeader.class);
 
     OwnershipVoucherHeader replaceHeader = new OwnershipVoucherHeader();
-    OwnerPublicKey ownerPublicKey = header.getPublicKey();
 
     replaceHeader.setDeviceInfo(header.getDeviceInfo());
     replaceHeader.setVersion(header.getVersion());
     replaceHeader.setCertHash(header.getCertHash());
 
     replaceHeader.setGuid(Guid.fromRandomUuid());
-    replaceHeader.setRendezvousInfo(header.getRendezvousInfo());
 
+    final Session session = HibernateUtil.getSessionFactory().openSession();
+    try {
+      final Transaction trans = session.beginTransaction();
+      OnboardingConfig onboardConfig =
+              session.get(OnboardingConfig.class, Long.valueOf(1));
+
+      if (onboardConfig.getReplacementRvInfo() == null) {
+        replaceHeader.setRendezvousInfo(header.getRendezvousInfo());
+      } else {
+        String body = onboardConfig.getReplacementRvInfo().getSubString(1,
+                Long.valueOf(onboardConfig.getReplacementRvInfo().length()).intValue());
+        RendezvousInfo replacement = Mapper.INSTANCE.readJsonValue(body, RendezvousInfo.class);
+        replaceHeader.setRendezvousInfo(replacement);
+      }
+      trans.commit();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      session.close();
+    }
+
+    OwnerPublicKey ownerPublicKey = header.getPublicKey();
     CryptoService cs = Config.getWorker(CryptoService.class);
     KeyResolver resolver = Config.getWorker(ReplacementKeySupplier.class).get();
     String alias = KeyResolver.getAlias(ownerPublicKey.getType(),
