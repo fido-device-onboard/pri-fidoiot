df846b17cc931577a9f9621d9c51187e0ca66f52
Benny <davis.benny@intel.com>
2024-05-07 20:29:13 +0530

    Connecting to Onboarding Manager on TO2 completion
      Connecting to Onboarding Manager on TO2 completion.
    
    Signed-off-by: Benny <davis.benny@intel.com>

---
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/GrpcOnboardingManager.java
@@ -0,0 +1,9 @@
+package org.fidoalliance.fdo.protocol;
+
+public class GrpcOnboardingManager implements OnboardingManagerConnection {
+
+    public void connect(String serialNumber, boolean status) {
+        // Write implementation for GRPC connection
+    }
+
+}
---
+++ protocol/src/main/java/org/fidoalliance/fdo/protocol/RestOnboardingManager.java
@@ -0,0 +1,8 @@
+package org.fidoalliance.fdo.protocol;
+
+public class RestOnboardingManager implements OnboardingManagerConnection {
+
+        public void connect(String serialNumber, boolean status) {
+            // Write implementation for REST connection
+        }
+}
